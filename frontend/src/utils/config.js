df51e9eaa2341f97d732589450fdcb5a9b3bf0c3
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-04-28 02:43:03 +0530

    modified base url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "https://api-alascafashion.herokuapp.com/api/v1/";
 // export const BASE_URL = "https://alascafashion-api.vercel.app/api/v1/";
-// export const BASE_URL = "http://alascafashion.in/api/v1/";
+// export const BASE_URL = "/api/v1/";
 
-export const BASE_URL = "/api/v1/";
+export const BASE_URL = "http://alascafashion.in/api/v1/";
 // export const BASE_URL = "http://localhost:4000/api/v1/";
